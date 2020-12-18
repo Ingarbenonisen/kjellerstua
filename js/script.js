8fe89b536e880fbf0f8d7719874c8820c4faa423
Ingarbenonisen <70366351+Ingarbenonisen@users.noreply.github.com>
2020-12-19 00:34:21 +0100

    new updt

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var spillerListe = [];
+
+function leggTilSpiller () {
+    
+    var spillerNavnInput = document.getElementById("spillerNavn").value;
+    
+    spillerListe.push(spillerNavnInput);
+
+    var navn = "";
+
+    for (i= 0; i < spillerListe.length; i++){
+        navn = navn + spillerListe[i] + "<br>";
+    }
+    document.getElementById("spillerListe").innerHTML = navn + "\n";
+    document.getElementById("spillerNavn").value = "";
+
+}
+
+var randomNavn = spillerListe [Math.floor(Math.random()*spillerListe.length)];
+document.getElementById("spillerNavnPrint").value = randomNavn;
